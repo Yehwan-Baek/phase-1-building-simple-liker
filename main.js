2636d85aca26e4d18a53ac0f744345fe05a6dc56
Yehwan Baek <baekokok98@gmail.com>
2023-03-23 12:11:18 +1100

    added code

--- main.js
+++ main.js
@@ -3,9 +3,31 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const heartIcon = document.querySelectorAll(".like-glyph")
 
+function liker(e) {
+  let heart = e.target;
+  mimicServerCall("url")
+  .then(()=>{
+    if(heart.innerHTML === EMPTY_HEART){
+      heart.innerHTML = FULL_HEART;
+      heart.className = "activated-heart";
+    } else {
+      heart.innerHTML = EMPTY_HEART;
+      heart.className = "";
+    }
+  })
+  .catch((err)=>{
+    let modal = document.querySelector("#modal")
+    modal.innerHTML = err;
+    modal.className = "";
+    setTimeout(()=>{modal.className = "hidden"},3000)
+  })
+}
 
-
+for(let likeHeart of heartIcon){
+  likeHeart.addEventListener("click", liker)
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
@@ -14,7 +36,7 @@ const FULL_HEART = '♥'
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+      const isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
